7be46baeacec9e31446a5566c699d7b6949319f8
Marianna <marianna.estrina@gmail.com>
2020-06-17 16:59:52 +0300

    task2

---
+++ learnJava/src/main/java/ru/stqa/pft/learnjava/MainClass.java
@@ -0,0 +1,23 @@
+package ru.stqa.pft.learnjava;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        Point p1 = new Point();
+        p1.name = 6;
+        p1.age =7;
+        Point p2 = new Point();
+        p2.name =3;
+        p2.age =7;
+
+
+        System.out.println("Distance between p1 and p2 = " + distance(p1, p2));
+    }
+    public static double distance(Point p1, Point p2) {
+
+        return Math.sqrt((p1.name -p2.name) *(p1.name -p2.name)+(p1.age -p2.age) *(p1.age -p2.age));
+
+    }
+
+}
+
--- learnJava/src/main/java/ru/stqa/pft/learnjava/MyFirstProgram.java
+++ learnJava/src/main/java/ru/stqa/pft/learnjava/MyFirstProgram.java
@@ -1,8 +1,19 @@
-package ru.stqa.pft.learnjava;
-
-public class MyFirstProgram {
-    public static void main(String[] args) {
-
-        System.out.println("Hello, Home!");
-    }
-}
+//package ru.stqa.pft.learnjava;
+//
+//public class MyFirstProgram {
+//    public static void main(String[] args) {
+//        Dog dog1 = new Dog();
+//        dog1.name = "Max";
+//        dog1.age =10;
+//        Dog dog2 = new Dog();
+//        dog2.name ="Bella";
+//        Dog dog3 = new Dog();
+//        dog3.name = "Jack";
+//        System.out.println("the dog is : " + dog1.name + " his age is :" + dog1.age);
+//    }
+//
+//    public static class Dog {
+//        public String name;
+//        public int age ;
+//    }
+//}
