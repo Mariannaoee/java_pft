2b3fe53a3fd48b6591df9f4408e6d4fcbd6f4191
Marianna <marianna.estrina@gmail.com>
2020-06-21 20:31:11 +0300

    hello home

--- learnJava/src/main/java/ru/stqa/pft/learnjava/MainClass.java
+++ learnJava/src/main/java/ru/stqa/pft/learnjava/MainClass.java
@@ -5,7 +5,7 @@ public class MainClass {
     public static void main(String[] args) {
         Point p1 = new Point(0,0);
         Point p2 = new Point(10,10);
-        System.out.println(p1.distance(p1,p2));
+        System.out.println(p1.distance(p2));
     }
 
 
--- learnJava/src/main/java/ru/stqa/pft/learnjava/Point.java
+++ learnJava/src/main/java/ru/stqa/pft/learnjava/Point.java
@@ -9,7 +9,7 @@ public class Point {
         this.x = x1;//x attribute, x1 variable like argument of function
         this.y = y1;
     }
-    public  double distance(Point p1, Point p2) {
+    public  double distance( Point p2) {
         return Math.sqrt((p2.x - this.x) * (p2.x - this.x) + (p2.y - this.y) * (p2.y - this.y));
     }
 
