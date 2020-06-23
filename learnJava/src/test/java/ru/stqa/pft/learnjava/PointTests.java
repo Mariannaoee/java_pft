1aa77141e951b13b8f63173f53c5b3f9b4872609
Marianna <marianna.estrina@gmail.com>
2020-06-23 10:34:48 +0300

    test

--- learnJava/src/test/java/ru/stqa/pft/learnjava/PointTests.java
+++ learnJava/src/test/java/ru/stqa/pft/learnjava/PointTests.java
@@ -6,13 +6,13 @@ import org.testng.annotations.Test;
 public class PointTests {
     @Test
     public void testPoint() {
-        Point a = new Point();
-        Point b = new Point();
-        a.x =5;
-        a.y =7;
-        b.x =4;
-        b.y =2;
-        double distanceResult = a.distance(a, b);
+        Point p1 = new Point(5,2);
+        Point p2 = new Point(4,2);
+        p1.x =5;
+        p1.y =7;
+        p2.x =4;
+        p2.y =2;
+        double distanceResult = p1.distance(p2);
 
         Assert.assertEquals(distanceResult,5.0990195135927845);
 
--- learnJava/src/test/java/ru/stqa/pft/learnjava/SquareTests.java
+++ learnJava/src/test/java/ru/stqa/pft/learnjava/SquareTests.java
@@ -6,8 +6,7 @@ import org.testng.annotations.Test;
 public class SquareTests {
     @Test
     public void testArea() {
-        Square s = new Square();//create new square
-        s.l = 5;
+        Square s = new Square(5);//create new square
         Assert.assertEquals(s.area(), 25.0);
     }
 
