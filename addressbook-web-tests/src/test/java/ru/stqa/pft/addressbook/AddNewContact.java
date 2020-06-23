554dc8238eb9c802f9508adba83fd7f08002eb70
Marianna <marianna.estrina@gmail.com>
2020-06-23 18:33:30 +0300

    task4

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/AddNewContact.java
@@ -0,0 +1,97 @@
+package ru.stqa.pft.addressbook;
+
+
+import java.util.concurrent.TimeUnit;
+
+import org.testng.annotations.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+
+public class AddNewContact {
+    private WebDriver driver;
+
+
+    @BeforeClass(alwaysRun = true)
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.get("http://localhost/addressbook/");
+
+        Login("admin", "secret");
+    }
+
+    private void Login(String username, String password) {
+        driver.get("http://localhost/addressbook/edit.php");
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys(username);
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys(password);
+        driver.findElement(By.xpath("//input[@value='Login']")).click();
+    }
+
+    @Test
+    public void testAddNewContact() throws Exception {
+        gotoNewContactPage();
+        fillContactForm(new ContactData("Marianna", "Estrina", "Holon", "087654324", "mariannaoe@mail.ru"));
+        submitContactCreation();
+        returnHomePage();
+    }
+
+    private void returnHomePage() {
+        driver.findElement(By.linkText("home")).click();
+    }
+
+    private void submitContactCreation() {
+        driver.findElement(By.xpath("(//input[@name='submit'])[2]")).click();
+    }
+
+    private void fillContactForm(ContactData contactData) {
+        driver.findElement(By.name("firstname")).click();
+        driver.findElement(By.name("firstname")).clear();
+        driver.findElement(By.name("firstname")).sendKeys(contactData.getFirstName());
+        driver.findElement(By.name("lastname")).click();
+        driver.findElement(By.name("lastname")).clear();
+        driver.findElement(By.name("lastname")).sendKeys(contactData.getLastName());
+        driver.findElement(By.name("address")).click();
+        driver.findElement(By.name("address")).clear();
+        driver.findElement(By.name("address")).sendKeys(contactData.getAddress());
+        driver.findElement(By.name("mobile")).click();
+        driver.findElement(By.name("mobile")).clear();
+        driver.findElement(By.name("mobile")).sendKeys(contactData.getPhone());
+        driver.findElement(By.name("email")).click();
+        driver.findElement(By.name("email")).clear();
+        driver.findElement(By.name("email")).sendKeys(contactData.getEmail());
+    }
+
+    private void gotoNewContactPage() {
+        driver.findElement(By.linkText("add new")).click();
+    }
+
+    @AfterClass(alwaysRun = true)
+    public void tearDown() throws Exception {
+        driver.quit();
+
+    }
+
+    private boolean isElementPresent(By by) {
+        try {
+            driver.findElement(by);
+            return true;
+        } catch (NoSuchElementException e) {
+            return false;
+        }
+    }
+
+    private boolean isAlertPresent() {
+        try {
+            driver.switchTo().alert();
+            return true;
+        } catch (NoAlertPresentException e) {
+            return false;
+        }
+    }
+
+
+}
