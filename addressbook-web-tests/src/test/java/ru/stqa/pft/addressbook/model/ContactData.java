251a0ea46601decb9251de320945355f97339cd8
Marianna <marianna.estrina@gmail.com>
2020-07-20 11:36:08 +0300

    ss

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -8,10 +8,7 @@ public class ContactData {
     private int id;
     private final String firstname;
     private final String surname;
-    private final String address;
-    private final String mobile;
-    private final String email;
-    private String group;
+
 
     public void setId(int id) {
         this.id = id;
@@ -26,20 +23,13 @@ public class ContactData {
         this.id = id;
         this.firstname = firstname;
         this.surname = surname;
-        this.address = address;
-        this.mobile = mobile;
-        this.email = email;
-        this.group = group;
     }
 
     public ContactData(String firstname, String surname, String address, String mobile, String email, String group) {
         this.id = 0;
         this.firstname = firstname;
         this.surname = surname;
-        this.address = address;
-        this.mobile = mobile;
-        this.email = email;
-        this.group = group;
+
     }
 
     public String getFirstname() {
@@ -50,21 +40,7 @@ public class ContactData {
         return surname;
     }
 
-    public String getAddress() {
-        return address;
-    }
 
-    public String getMobile() {
-        return mobile;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getGroup() {
-        return group;
-    }
 
 
 
@@ -73,12 +49,9 @@ public class ContactData {
         return "ContactData{" +
                 "id='" + id + '\'' +
                 ", firstname='" + firstname + '\'' +
-                ", surname='" + surname + '\'' +
-                ", address='" + address + '\'' +
-                ", mobile='" + mobile + '\'' +
-                ", email='" + email + '\'' +
-                ", group='" + group + '\'' +
-                '}';
+                ", surname='" + surname + '\''
+
+               ;
     }
 
 
@@ -89,13 +62,11 @@ public boolean equals(Object o) {
         ContactData that = (ContactData) o;
         return id == that.id &&
         Objects.equals(firstname, that.firstname) &&
-        Objects.equals(surname, that.surname) &&
-        Objects.equals(address, that.address) &&
-        Objects.equals(mobile, that.mobile) &&
-        Objects.equals(email, that.email) ;
+        Objects.equals(surname, that.surname);
+
         }
 
 @Override
 public int hashCode() {
-        return Objects.hash(id, firstname, surname, address, mobile, email);
+        return Objects.hash(id, firstname, surname);
         }}
